573e0ecd66c6c31237262d435227f68a026d0a99
WessCoby <cobygiven@gmail.com>
2022-06-07 16:55:31 +0000

    Fixed issue with text overflowing the container

--- src/components/search-result.tsx
+++ src/components/search-result.tsx
@@ -5,26 +5,22 @@ import { AddressData } from '../types';
 interface ResultItemProps {
   title: string;
   value: string;
-  border?: boolean;
+	className?: string;
 }
 
 interface SearchResultProps {
   data: AddressData;
 }
 
-const Result: FC<ResultItemProps> = ({ title, value, border = false }) => (
-  <div
-    className={
-      `flex flex-col items-center flex-1 my-1 ${border && 'lg:border-r'} lg:items-start lg:mx-3 xl:mx-4 h-1/4 lg:py-5`
-    }
-  >
+const Result: FC<ResultItemProps> = ({ title, value, className = '' }) => (
+  <div className={` ${className}`}>
     <p
-      className="text-xs font-semibold tracking-widest text-gray-500 uppercase"
+      className="text-xs text-center lg:text-left font-semibold tracking-widest text-gray-500 uppercase"
     >
         {title}
     </p>
     <h2
-      className="pr-2 text-xl font-semibold tracking-widest md:text-2xl lg:text-xl xl:text-2xl"
+      className="pr-2 text-lg md:text-xl font-semibold tracking-widest text-center lg:text-left lg:mt-1"
     >
       {value}
     </h2>
@@ -39,15 +35,15 @@ const SearchResult: FC<SearchResultProps> = ({ data }) => {
 
   return (
     <div
-      className="absolute top-0 z-10 flex items-center justify-center w-full h-64 -mt-20 sm:-mt-16 lg:h-32"
+      className="absolute top-0 z-10 flex items-center justify-center w-full h-auto -mt-20 sm:-mt-16 lg:h-32"
     >
       <div
-        className="flex flex-col items-center justify-center w-10/12 h-full p-2 bg-white rounded-lg shadow-xl sm:w-2/3 lg:w-4/5 xl:w-3/4 lg:flex-row lg:justify-around lg:items-stretch"
+				className="bg-white w-10/12 h-full shadow-xl rounded-lg lg:grid lg:grid-cols-4 lg:divide-x py-4 lg:py-6 lg:px-4"
       >
-        <Result title="Ip Address" value={ip} border />
-        <Result title="Location" value={loc} border />
-        <Result title="Timezone" value={tz} border />
-        <Result title="ISP" value={isp} />
+        <Result title="Ip Address" value={ip} />
+        <Result title="Location" value={loc} className="lg:pl-6 mt-4 lg:mt-0" />
+        <Result title="Timezone" value={tz} className="lg:pl-6 mt-4 lg:mt-0" />
+        <Result title="ISP" value={isp} className="lg:pl-6 mt-4 lg:mt-0" />
       </div>
     </div>
   )
